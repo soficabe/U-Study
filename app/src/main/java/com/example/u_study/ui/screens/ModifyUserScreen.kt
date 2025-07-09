8bdf77319eb3481ebf07ab9970657aff96866c1e
Sofia <irenesofia.lotti@studio.unibo.it>
2025-07-09 17:36:22 +0200

    merge conflict resolved

---
+++ app/src/main/java/com/example/u_study/ui/screens/ModifyUserScreen.kt
@@ -0,0 +1,115 @@
+package com.example.u_study.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.u_study.ui.composables.AppBar
+import com.example.u_study.ui.composables.NavigationBar
+import com.example.u_study.ui.composables.ProfileIcon
+import com.example.u_study.ui.composables.SaveButton
+
+@Composable
+fun ModifyUserScreen() {
+    var firstName by rememberSaveable { mutableStateOf("Sofia") }
+    var lastName by rememberSaveable { mutableStateOf("Bianchi") }
+    var email by rememberSaveable { mutableStateOf("sofiabianchi@gmail.com") }
+
+    val scrollState = rememberScrollState()
+    Scaffold(
+        topBar = { AppBar(title = "Edit Profile") },
+        bottomBar = { NavigationBar() }
+    ) {
+        innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(horizontal = 24.dp)
+                .verticalScroll(scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Spacer(Modifier.height(24.dp))
+
+            //immagine
+            ProfileIcon()
+
+            Spacer(Modifier.height(32.dp))
+
+            //column creata solo per allineare a sinistra
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.Start
+            ) {
+                //first name
+
+                Text("First Name",
+                    style = MaterialTheme.typography.labelLarge,
+                    fontWeight = FontWeight.SemiBold)
+                Spacer(Modifier.height(4.dp))
+                OutlinedTextField(
+                    value = firstName,
+                    onValueChange = { firstName = it },
+                    modifier = Modifier.fillMaxWidth(),
+                    enabled = true,
+                    singleLine = true
+                )
+
+                Spacer(Modifier.height(16.dp))
+                //last name
+
+                Text("Last Name",
+                    style = MaterialTheme.typography.labelLarge,
+                    fontWeight = FontWeight.SemiBold)
+                Spacer(Modifier.height(4.dp))
+                OutlinedTextField(
+                    value = lastName,
+                    onValueChange = { lastName = it },
+                    modifier = Modifier.fillMaxWidth(),
+                    enabled = true,
+                    singleLine = true
+                )
+
+                Spacer(Modifier.height(16.dp))
+                //email
+
+                Text("Email",
+                    style = MaterialTheme.typography.labelLarge,
+                    fontWeight = FontWeight.SemiBold)
+                Spacer(Modifier.height(4.dp))
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    modifier = Modifier.fillMaxWidth(),
+                    enabled = true,
+                    singleLine = true
+                )
+
+            }
+
+            Spacer(Modifier.height(32.dp))
+
+            SaveButton("Save changes")
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/u_study/ui/screens/ProfileScreen.kt
@@ -0,0 +1,130 @@
+package com.example.u_study.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ShowChart
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.u_study.ui.composables.AppBar
+import com.example.u_study.ui.composables.NavigationBar
+import com.example.u_study.ui.composables.ProfileIcon
+import com.example.u_study.ui.composables.SaveButton
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileScreen() {
+    val scrollState = rememberScrollState()
+    Scaffold(
+        topBar = { AppBar(title = "Profile") },
+        bottomBar = { NavigationBar() }
+    ) {
+        innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(horizontal = 24.dp)
+                .verticalScroll(scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(Modifier.height(24.dp))
+
+            //immagine VA MODIFICATA PER RETTANGOLARI? :(
+            ProfileIcon()
+
+            Spacer(Modifier.height(16.dp))
+
+            //nome cognome
+            Text(text = "Sofia Bianchi",
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold)
+
+            Spacer(Modifier.height(24.dp))
+
+            //bottoni
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally)
+            ) {
+                ProfileButton(icon = Icons.Outlined.Favorite, text = "Favorite Libraries", onClick = { /* ci penseremo poi */ })
+                ProfileButton(icon = Icons.AutoMirrored.Outlined.ShowChart, text = "Stats", onClick = { /* ci penseremo poi */ })
+            }
+
+            Spacer(Modifier.height(32.dp))
+
+            //mail
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text("Email",
+                    style = MaterialTheme.typography.labelLarge,
+                    fontWeight = FontWeight.SemiBold)
+                Spacer(Modifier.height(4.dp))
+                OutlinedTextField(
+                    value = "sofiabianchi@gmail.com",
+                    onValueChange = {},
+                    modifier = Modifier.fillMaxWidth(),
+                    enabled = false,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        disabledTextColor = MaterialTheme.colorScheme.onSurface,
+                        disabledBorderColor = MaterialTheme.colorScheme.outline,
+                        disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ) //ho cambiato i colori visto che con enabled=false li rovinava
+                )
+            }
+
+            Spacer(Modifier.height(32.dp))
+
+            SaveButton(text = "Edit profile")
+
+            Spacer(Modifier.height(16.dp))
+
+        }
+    }
+}
+
+/* Sono i bottoni blu presenti sotto l'immagine profilo
+ */
+@Composable
+fun ProfileButton(icon: ImageVector, text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(12.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF03A9F4)), // Blu
+        modifier = Modifier.width(150.dp).height(80.dp)
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(imageVector = icon, contentDescription = null, tint = Color.White)
+            Spacer(Modifier.height(4.dp))
+            Text(text, color = Color.White, fontSize = 12.sp, fontWeight = FontWeight.Bold)
+        }
+    }
+}
